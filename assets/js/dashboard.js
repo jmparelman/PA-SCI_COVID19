d27bde9468876fe6499d89bdbad87d53965fb151
Matt O'Donnell <m.b.odonnell@gmail.com>
2020-12-04 00:12:14 -0500

    style update and get latest data from main branch

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -347,7 +347,7 @@ d3.csv("/PA-SCI_COVID19/data/latest_data/PA_DOC_testing_data.csv").then(function
 				        .text(start_date);
 
 				        svg.append('text')
-				        .attr('x', x(data[data.length-1].date)-24)
+				        .attr('x', x(data[data.length-1].date)-26)
 				        .attr('y', y(0)+6)
 				        .attr("dy", ".35em")
 				        .attr('class', 'datelabel')
