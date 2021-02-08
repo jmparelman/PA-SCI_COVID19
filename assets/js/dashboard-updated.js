c19484462b7c6331f6d6774e2427b6fd9b114b02
Matt O'Donnell <m.b.odonnell@gmail.com>
2021-02-07 20:35:10 -0500

    updated view updates

--- assets/js/dashboard-updated.js
+++ assets/js/dashboard-updated.js
@@ -289,7 +289,7 @@ d3.csv("https://raw.githubusercontent.com/jmparelman/PA-SCI_COVID19/main/data/la
 				      var width = 200;
 				        var height = 40;
 				        var x = d3.scaleLinear().range([0, width-2]);
-				        var y = d3.scaleLinear().range([0, height]);
+				        var y = d3.scaleLinear().range([height, 0]);
 
 
 
@@ -314,10 +314,6 @@ d3.csv("https://raw.githubusercontent.com/jmparelman/PA-SCI_COVID19/main/data/la
 
 								x.domain(d3.extent(data, function(d) { return d.date; }));
 								y.domain(d3.extent(data, function(d) { return d.incarcerated_person_active_cases; }));
-
-
-								console.log(d3.extent(data, function(d) { return d.incarcerated_person_active_cases; }));
-								console.log(y(function(d) { return y(d.incarcerated_person_active_cases); }));
 
 				        var max_idx = d3.maxIndex(data, d=>d.incarcerated_person_active_cases);
 				        var max_value = data[max_idx].incarcerated_person_active_cases;
