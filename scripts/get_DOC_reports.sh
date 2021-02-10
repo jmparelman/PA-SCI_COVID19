fb0965c0134e16fd3d1652b74a4fdcb83cff06ec
Matt O'Donnell <m.b.odonnell@gmail.com>
2021-02-10 16:01:00 -0500

    daily count script updates

--- scripts/get_DOC_reports.sh
+++ scripts/get_DOC_reports.sh
@@ -5,7 +5,7 @@ d=`date | tr ' ' '_'`
 cd ../data/DOC_daily_counts
 
 
-curl -o PA-DOC-WW-Report_${d}.pdf https://www.cor.pa.gov/Documents/WW-Report.pdf
+curl -o PA-DOC-WW-Report_${d}.pdf https://www.cor.pa.gov/Documents/WW-Report.pdf?t=${d}
 
-curl -o PA-DOC-COVID-19-Daily-Count-${d}.pdf https://www.cor.pa.gov/Documents/DOC-COVID-19-Daily-Count.pdf
+curl -o PA-DOC-COVID-19-Daily-Count-${d}.pdf https://www.cor.pa.gov/Documents/DOC-COVID-19-Daily-Count.pdf?t=${d}
 
